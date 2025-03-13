ba748bf28bf1d5e0054cfe8481e506a98e2b6f2b
AlbertoCadorin <alberto.cadori01@gmail.com>
2025-03-13 15:44:20 +0100

    feat: bonus, add button

--- js/app.js
+++ js/app.js
@@ -1,25 +1,32 @@
 const emailList = "https://flynn.boolean.careers/exercises/api/random/mail";
 // container per mettere le email
 const emailElement = document.getElementById("container-mail");
+const buttonElement = document.getElementById("button")
+
+reload()
 
 
-let email = "";
-// ciclo per creare 10 email 
-for (let i = 0; i < 10 ; i++) {
-    
-    axios.get(emailList)
-    .then(response =>{
+
+// FUNZIONE //
+
+// funzione per ricaricare la pagina 
+function reload(){
+    let email = "";
+    // ciclo per creare 10 email 
+        for (let i = 0; i < 10 ; i++) {
         
-        email = email + `<div> ${response.data.response}</div>`
-        console.log(response.data.response) 
-        emailElement.innerHTML = email
-   
-    })
-    .catch(error => {
-        console.log(error)
-    }) 
+            axios.get(emailList)
+            .then(response =>{
+            
+                email = email + `<div class="border p-3">${response.data.response}</div>`
+                console.log(response.data.response) 
+                emailElement.innerHTML = email
+                buttonElement.addEventListener('click',reload)
+       
+            })
+            .catch(error => {
+                console.log(error)
+            })
+        }
+    
 }
-
-
-
- 
