9e7634084b1375a8ee231aa20331502f1459c4c4
AlbertoCadorin <alberto.cadori01@gmail.com>
2025-03-13 14:36:49 +0100

    feat: add the cycle to generate 10 email

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
-console.log('ciaoooooooooooooo')
+const emailList = "https://flynn.boolean.careers/exercises/api/random/mail";
+// container per mettere le email
+const emailElement = document.getElementById("container-mail");
+
+
+let email = "";
+// ciclo per creare 10 email 
+for (let i = 0; i < 10 ; i++) {
+    
+    axios.get(emailList)
+    .then(response =>{
+        
+        email = email + `<div> ${response.data.response}</div>`
+        console.log(response.data.response) 
+        emailElement.innerHTML = email
+   
+    })
+    .catch(error => {
+        console.log(error)
+    }) 
+}
+
+
+
+ 
